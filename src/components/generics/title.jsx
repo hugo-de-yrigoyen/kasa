d780e5110939d8df45daacdecad18ee5b56a1dc9
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2023-03-09 14:19:25 +0100

    New fetch and components

---
+++ src/components/generics/title.jsx
@@ -0,0 +1,8 @@
+export default function Title(title, location) {
+  return (
+    <div className="bloc-title">
+      <h1>{title}</h1>
+      <p>{location}</p>
+    </div>
+  );
+}
---
+++ src/components/icons/star-full.jsx
@@ -0,0 +1,3 @@
+export default function StarFull() {
+  return <img src=".../assets/star-full.png" alt="Star" />;
+}
