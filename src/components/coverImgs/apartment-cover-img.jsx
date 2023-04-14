46dd2934c0dd2096b12ea2d7a130329db05e662d
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2023-04-14 09:46:57 +0200

    Gallery pictures

--- src/components/coverImgs/apartment-cover-img.jsx
+++ src/components/coverImgs/apartment-cover-img.jsx
@@ -1,14 +1,28 @@
 import "../../styles/cover-img.css";
 import arrowLeft from "../../assets/arrow-left.png";
 import arrowRight from "../../assets/arrow-right.png";
+import { useState } from "react";
 
 export default function ApartmentCoverImg({ apartment }) {
+  let [number, setImageNumber] = useState(0);
+
+  if (number < 0) {
+    number = 0;
+  }
+  if (number >= apartment.pictures.length) {
+    number = apartment.pictures.length - 1;
+  }
+
   return (
     <div className="cover-img">
-      <img src={apartment.cover} className="cover" alt="Cover" />
+      <img src={apartment.pictures[number]} className="cover" alt="Gallery" />
       <div className="arrows">
-        <img src={arrowLeft} alt="Left arrow" />
-        <img src={arrowRight} alt="Right arrow" />
+        <button id="leftArrow" onClick={() => setImageNumber(number - 1)}>
+          <img src={arrowLeft} alt="Left arrow" />
+        </button>
+        <button id="rightArrow" onClick={() => setImageNumber(number + 1)}>
+          <img src={arrowRight} alt="Right arrow" />
+        </button>
       </div>
     </div>
   );
