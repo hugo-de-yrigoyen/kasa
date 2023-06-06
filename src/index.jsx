b877ad1884162a1691bf0648406f5456d2b22295
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2023-06-06 16:21:39 +0200

    url correction

--- src/index.jsx
+++ src/index.jsx
@@ -9,16 +9,16 @@ import "./styles/index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "kasa/",
     element: <Home />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "about",
+    path: "kasa/about",
     element: <About />,
   },
   {
-    path: "apartments/:apartmentId",
+    path: "kasa/apartments/:apartmentId",
     element: <Apartment />,
     errorElement: <ErrorPage />,
   },
